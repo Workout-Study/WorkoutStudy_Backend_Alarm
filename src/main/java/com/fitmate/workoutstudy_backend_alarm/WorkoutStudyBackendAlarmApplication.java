a730605e5d518dc26c0e2223253d21e411d9de6c
Jake-huen <57055730+Jake-huen@users.noreply.github.com>
2024-06-12 19:33:20 +0900

    Init Project

---
+++ src/main/java/com/fitmate/workoutstudy_backend_alarm/WorkoutStudyBackendAlarmApplication.java
@@ -0,0 +1,13 @@
+package com.fitmate.workoutstudy_backend_alarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutStudyBackendAlarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutStudyBackendAlarmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fitmate/workoutstudy_backend_alarm/WorkoutStudyBackendAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitmate.workoutstudy_backend_alarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutStudyBackendAlarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
